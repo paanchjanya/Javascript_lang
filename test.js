07e35addb44c03be84d72a4d181639fad654509c
paanchjanya <desaipraveen22@gmail.com>
2024-10-04 20:10:39 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+"use strict";//newer version of javascript
+
+let name="praveen"
+let age=22
+let isLoggedIn=false
+
+// number=>2^53 
+// bigint
+// string
+// boolean=true/false
+// null=std alone value
+// undefined
+console.log(typeof undefined);
+console.log(typeof null);
+
